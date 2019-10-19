47f25f306fe5361bbee3c6a01a75233268df0955
Rijwan Hossain <rijyan.cse.152@gmail.com>
2019-10-19 18:29:41 +0600

    Create app.js
    The main code is here.

---
+++ app.js
@@ -0,0 +1,50 @@
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video 
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.79,    // confidence threshold for predictions.
+} 
+  
+
+navigator.getUserMedia = 
+    navigator.getUserMedia || 
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+const video = document.querySelector('#video') 
+const audio = document.querySelector('#audio') 
+const canvas = document.querySelector('#canvas') 
+const context = canvas.getContext('2D') 
+let model;
+
+handTrack.startVideo(video) 
+    .then(status => { 
+        if(status) { 
+            navigator.getUserMedia({video: {}}, 
+                stream => { 
+                    video.srcObject = stream 
+                    setInterval(runDetection, 1000) 
+                }, 
+                err => console.log('Error Occured') 
+            ) 
+        } 
+    }) 
+
+function runDetection() { 
+    model.detect(video) 
+        .then(result => { 
+            if(result.length > 0) { 
+                audio.play() 
+            } 
+        }) 
+} 
+
+handTrack.load(modelParams) 
+    .then(lmodel => { 
+        model = lmodel 
+    }) 
+    .catch(err => console.log('Error Occured')) 
+
+// audio.play() 
